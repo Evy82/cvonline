ddc3a71d71705b6bf5e4887c60af646035e7546f
DESKTOP-KPG23DQ\Eva <matscomidas@hotmail.com>
2023-01-16 22:20:50 -0300

    Este es el proyecto final de Eva Medrano para AP 4.0

---
+++ codigo.js
@@ -0,0 +1,33 @@
+// INICIO
+
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
+
+  if (navMenu.classList.contains("nav-menu_visible")) {
+    navToggle.setAttribute("aria-label", "Cerrar menú");
+  } else {
+    navToggle.setAttribute("aria-label", "Abrir menú");
+  }
+});
+
+
+// INFORMACION PERSONAL
+
+let readMore_btn = document.getElementById('readMore_btn');
+let hideText = document.getElementById('hideText');
+
+readMore_btn.addEventListener('click', toggleText);
+
+function toggleText() {
+  hideText.classList.toggle('showText');
+
+  if(hideText.classList.contains('showText')) {
+    readMore_btn.innerHTML = 'Leer menos'
+  }
+  else {
+    readMore_btn.innerHTML = 'Leer más'
+  }
+}
